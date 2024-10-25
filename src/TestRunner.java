e82d07dbe5a555125331bb31a8d0e3b3b4da6ca2
BT_1N3_17 <sample@bths.edu>
2024-10-25 13:13:16 -0400

    print statements

--- src/TestRunner.java
+++ src/TestRunner.java
@@ -1,13 +1,20 @@
+
 public class TestRunner {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("I hate Mondays");
         int x = 5;
-        x = +10;
+        x += 10;
         x++;
         System.out.println("X is now" + x);
-        if (x < 10) {
+        if (x > 10) {
             System.out.println("Ye");
         } else System.out.println("na");
+
+        System.out.println("fun with git");
+        System.out.println(x + 15);
+        System.out.println("YEEEAAAAAA FRIDAY!!!!!");
+        System.out.println();
     }
+
 }
