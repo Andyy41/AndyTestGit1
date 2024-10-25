4e3fe9d4496a23d65f1d32a377626914b92676e0
BT_1N3_17 <sample@bths.edu>
2024-10-25 13:10:48 -0400

    Added some logic

--- src/TestRunner.java
+++ src/TestRunner.java
@@ -1,5 +1,13 @@
 public class TestRunner {
     public static void main(String[] args) {
-
+        System.out.println("Hello World!");
+        System.out.println("I hate Mondays");
+        int x = 5;
+        x = +10;
+        x++;
+        System.out.println("X is now" + x);
+        if (x < 10) {
+            System.out.println("Ye");
+        } else System.out.println("na");
     }
 }
